ae9e5ed9ba0fae78d3a975dec72090af8b43a99f
Victor Manuel <mbvictorsantos@gmail.com>
2023-05-31 12:11:58 -0300

    feat: Added 2SAT

---
+++ code/graph/2sat.cpp
@@ -0,0 +1,119 @@
+// 2SAT
+//
+// verifica se existe e encontra solução
+// para fórmulas booleanas da forma
+// (a or b) and (!a or c) and (...)
+// 
+// indexado em 0
+// n(a) = 2*x e n(~a) = 2*x+1
+// a = 2 ; n(a) = 4 ; n(~a) = 5 ; n(a)^1 = 5 ; n(~a)^1 = 4
+// 
+// https://cses.fi/problemset/task/1684/
+// https://codeforces.com/gym/104120/problem/E
+// (add_eq, add_true, add_false e at_most_one não foram testadas)
+// 
+// O(n + m)
+
+struct sat {
+    int n, tot;
+    vector<vector<int>> adj, adjt; // grafo original, grafo transposto
+    vector<int> vis, comp, ans;
+    stack<int> topo; // ordem topológica
+
+    sat() {}
+    sat(int n_) : n(n_), tot(n), adj(2*n), adjt(2*n) {}
+
+    void dfs(int x) {
+        vis[x] = true;
+
+        for (auto e : adj[x]) {
+            if (!vis[e]) dfs(e);
+        }
+
+        topo.push(x);
+    }
+
+    void dfst(int x, int& id) {
+        vis[x] = true;
+        comp[x] = id;
+
+        for (auto e : adjt[x]) {
+            if (!vis[e]) dfst(e, id);
+        }
+    }
+
+    void add_impl(int a, int b) { // a -> b = (!a or b)
+        a = (a >= 0 ? 2*a : -2*a-1);
+        b = (b >= 0 ? 2*b : -2*b-1);
+
+        adj[a].push_back(b);
+        adj[b^1].push_back(a^1);
+
+        adjt[b].push_back(a);
+        adjt[a^1].push_back(b^1);
+    }
+
+    void add_or(int a, int b) { // a or b
+        add_impl(~a, b);
+    }
+
+    void add_and(int a, int b) { // a and b
+        add_or(a, b), add_or(~a, b), add_or(a, ~b);
+    }
+
+    void add_xor(int a, int b) { // a xor b = (a != b)
+        add_or(a, b), add_or(~a, ~b);
+    }
+
+    void add_eq(int a, int b) { // a = b
+        add_xor(~a, b);
+    }
+
+    void add_true(int a) { // a = T
+        add_impl(~a, a);
+    }
+
+    void add_false(int a) { // a = F
+        add_impl(a, ~a);
+    }
+
+    // magia - brunomaletta
+    void at_most_one(vector<int> v) { // no max um verdadeiro
+        adj.resize(2*(tot+v.size()));
+        for (int i = 0; i < v.size(); i++) {
+            add_impl(tot+i, ~v[i]);
+            if (i) {
+                add_impl(tot+i, tot+i-1);
+                add_impl(v[i], tot+i-1);
+            }
+        }
+        tot += v.size();
+    }
+
+    pair<bool, vector<int>> solve() {
+        ans.assign(n, -1);
+        comp.assign(2*tot, -1);
+        vis.assign(2*tot, 0);
+        int id = 1;
+
+        for (int i = 0; i < 2*tot; i++) if (!vis[i]) dfs(i);
+        
+        vis.assign(2*tot, 0);
+        while (topo.size()) {
+            auto x = topo.top();
+            topo.pop();
+
+            if (!vis[x]) {
+                dfst(x, id);
+                id++;
+            }
+        }
+
+        for (int i = 0; i < tot; i++) {
+            if (comp[2*i] == comp[2*i+1]) return {false, {}};
+            ans[i] = (comp[2*i] > comp[2*i+1]);
+        }
+
+        return {true, ans};
+    }
+};
